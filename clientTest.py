42502f94b4dcf3410fe0ccb156f9ccedd5ad050b
Pranav Srinivasan <pranavsrinivasan@Pranavs-MacBook-Air.local>
2022-10-19 16:37:21 -0700

    Add serverTest and clientTest files

--- clientTest.py
+++ clientTest.py
@@ -1,6 +1,6 @@
 import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('0.0.0.0', 8080))
+client.connect(('172.30.77.131', 8080))
 client.sendall('I am CLIENT\n'.encode())
 from_server = client.recv(4096)
 client.close()
